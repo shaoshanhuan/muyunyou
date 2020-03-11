72343de86a0cf081ac28e43ac416c023360ebaeb
邵山欢 <shaoshanhuan@163.com>
2020-03-11 08:27:13 +0800

    2020年3月11日 早

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,24 +1,41 @@
-(function(){
-    // 菜单代码
-    // 得到菜单触碰项a标签
-    var menu_as = document.querySelectorAll('#v-menu a[data-n]');
+// 垂直菜单的实现
+(function () {
+    // 得到所有菜单触碰项li标签
+    var menu_lis = document.querySelectorAll('#v-menu li[data-n]');
+    // 得到vmenubox盒子
+    var vmenubox = document.querySelector('#v-menu-box');
+    // 得到所有menu菜单
+    var menus = document.querySelectorAll('#menus .menu');
+
     // 批量添加监听
-    for(var i = 0 ; i<menu_as.length ; i++){
-        (function(i){
-            menu_as[i].onmouseenter = function(){
-                // 触碰项的n
-                var n = this.getAttribute('data-n');
-                // 寻找相应的menu，让它显示
-                var menu = document.querySelector('#menus .menu[data-n=' + n + ']');
-                menu.className = "menu active";
-            }
-            menu_as[i].onmouseleave = function(){
-                // 触碰项的n
-                var n = this.getAttribute('data-n');
-                // 寻找相应的menu，让它隐藏
-                var menu = document.querySelector('#menus .menu[data-n=' + n + ']');
-                menu.className = "menu";
+    for (var i = 0; i < menu_lis.length; i++) {
+        (function (i) {
+            // 鼠标触碰某个菜单项
+            menu_lis[i].onmouseenter = function () {
+                // 所有菜单项去掉active类
+                for (var j = 0; j < menus.length; j++) {
+                    menu_lis[j].className = '';
+                }
+                // 自己加active类
+                this.className = 'active';
+                // 让所有菜单隐藏，去掉active类
+                for (var j = 0; j < menus.length; j++) {
+                    menus[j].className = 'menu';
+                }
+                // 让序号相同的菜单项添加menu类
+                menus[i].className = 'menu active';
             }
         })(i)
+    }
+    // 鼠标离开整个vmenubox盒子
+    vmenubox.onmouseleave = function () {
+        // 让所有菜单隐藏
+        for (var j = 0; j < menus.length; j++) {
+            menus[j].className = 'menu';
+        }
+        // 所有菜单项去掉active类
+        for (var j = 0; j < menus.length; j++) {
+            menu_lis[j].className = '';
+        }
     }
 })();
